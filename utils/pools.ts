fd9233d06d816b4c6044f0d10e4ee40649323534
Sotiris Blad <sotiris2009@hotmail.com>
2021-05-02 03:40:04 +0300

    Update pools.ts

--- utils/pools.ts
+++ utils/pools.ts
@@ -1,4 +1,9 @@
 // List of pools + start/end block.
 export const pools = [
+  {
+    address: "0x04a8875a5fb669bf05ccc0af62fdbed93202afc8 ",
+    startBlock: 6638200,
+    endBlock: 10366200,
+  },
 
 ];
