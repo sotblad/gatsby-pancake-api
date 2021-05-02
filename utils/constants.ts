c577e14a113edf7ec3eb6febf2c4722dca80a74c
Sotiris Blad <sotiris2009@hotmail.com>
2021-05-02 03:27:31 +0300

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,13 +1,13 @@
 import { ChainId, Token } from "@pancakeswap-libs/sdk";
 
 // BEP-20 addresses.
-export const CAKE = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82";
+export const CAKE = "0x04a8875a5fb669bf05ccc0af62fdbed93202afc8";
 export const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 export const DEAD = "0x000000000000000000000000000000000000dEaD";
 
 // Contract addresses.
-export const CAKE_BNB_FARM = "0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6";
-export const MASTERCHEF_CONTRACT = "0x73feaa1eE314F8c655E354234017bE2193C9E24E";
+export const CAKE_BNB_FARM = "0x2E776e7cDC3E1c8b3d315140c8Ebb10E765ce30a";
+export const MASTERCHEF_CONTRACT = "0xa12c74f1d8e17d14a4ddb50b39882175cf5d02ec";
 export const LOTTERY_CONTRACT = "0x3C3f2049cc17C136a604bE23cF7E42745edf3b91";
 export const MULTICALL_CONTRACT = "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb";
 
